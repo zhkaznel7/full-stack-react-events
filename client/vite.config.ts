78a4e04a7e9201cbb2a3f29c5588188bc4f8737b
zhanel <s.aizhan.b@mail.ru>
2026-05-11 14:10:21 +0500

    new

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,9 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   resolve:{
